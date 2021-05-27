2234d7ad0bb0d3494c4a9e4abab05a5b56ad5ac2
Ambrose Chua <ambrose@hey.com>
2021-05-27 16:03:30 +0800

    Fix and write tests for Prepare()

--- builder/common/access_config_test.go
+++ builder/common/access_config_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/hashicorp/packer-plugin-sdk/common"
 )
 
 func TestAccessConfigPrepare_Region(t *testing.T) {
@@ -37,11 +38,31 @@ func TestAccessConfigPrepare_RegionRestricted(t *testing.T) {
 		Region: aws.String("us-gov-west-1"),
 	}))
 
-	if err := c.Prepare(); err != nil {
+	packerConfig := &common.PackerConfig{
+		PackerCoreVersion: "0.0.0",
+	}
+	if err := c.Prepare(packerConfig); err != nil {
 		t.Fatalf("shouldn't have err: %s", err)
 	}
 
 	if !c.IsGovCloud() {
 		t.Fatal("We should be in gov region.")
+	}
+}
+
+func TestAccessConfigPrepare_UnknownPackerCoreVersion(t *testing.T) {
+	c := FakeAccessConfig()
+
+	// Create a Session with a custom region
+	c.session = session.Must(session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1"),
+	}))
+
+	if err := c.Prepare(nil); err != nil {
+		t.Fatalf("shouldn't have err: %s", err)
+	}
+
+	if c.packerConfig.PackerCoreVersion != "unknown" {
+		t.Fatalf("packer core version should be unknown, but got %s", c.packerConfig.PackerCoreVersion)
 	}
 }
