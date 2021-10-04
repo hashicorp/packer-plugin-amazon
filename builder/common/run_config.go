eab9d3b82069b31a1419adb824195de81cef1ec9
Wilken Rivera <wilken@hashicorp.com>
2021-10-04 15:41:18 -0400

    Fix generated partials

--- builder/common/run_config.go
+++ builder/common/run_config.go
@@ -243,7 +243,7 @@ type RunConfig struct {
 	//   source_ami_filter {
 	//     filters = {
 	//        virtualization-type = "hvm"
-	//        name = "ubuntu/images/\*ubuntu-xenial-16.04-amd64-server-\*"
+	//        name = "ubuntu/images/*ubuntu-xenial-16.04-amd64-server-*"
 	//        root-device-type = "ebs"
 	//     }
 	//     owners = ["099720109477"]
@@ -260,7 +260,7 @@ type RunConfig struct {
 	//     "source_ami_filter": {
 	//        "filters": {
 	//        "virtualization-type": "hvm",
-	//        "name": "ubuntu/images/\*ubuntu-xenial-16.04-amd64-server-\*",
+	//        "name": "ubuntu/images/*ubuntu-xenial-16.04-amd64-server-*",
 	//        "root-device-type": "ebs"
 	//        },
 	//        "owners": ["099720109477"],
