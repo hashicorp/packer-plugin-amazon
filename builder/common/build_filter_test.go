9dc4f4cb06c55c2f79352a2b3e9245e05a10a75f
Dennis Carey <dwc0011@users.noreply.github.com>
2022-01-19 17:14:03 -0500

    Add test for building from list value, update test for single value to test trimming leading spaces

--- builder/common/build_filter_test.go
+++ builder/common/build_filter_test.go
@@ -1,24 +1,83 @@
 package common
 
 import (
+	"strings"
 	"testing"
 )
 
-func TestStepSourceAmiInfo_BuildFilter(t *testing.T) {
+func TestStepSourceAmiInfo_BuildFilter_SingleValue(t *testing.T) {
 	filter_key := "name"
 	filter_value := "foo"
 	filter_key2 := "name2"
-	filter_value2 := "foo2"
+	filter_value2 := " foo2"
+	filter_key3 := "name3"
+	filter_value3 := "foo3 "
 
-	inputFilter := map[string]string{filter_key: filter_value, filter_key2: filter_value2}
+	inputFilter := map[string]string{
+		filter_key:  filter_value,
+		filter_key2: filter_value2,
+		filter_key3: filter_value3,
+	}
+
 	outputFilter := buildEc2Filters(inputFilter)
+
+	testFilter := map[string]string{
+		filter_key:  filter_value,
+		filter_key2: strings.TrimSpace(filter_value2),
+		filter_key3: filter_value3,
+	}
 
 	// deconstruct filter back into things we can test
 	foundMap := map[string]bool{filter_key: false, filter_key2: false}
 	for _, filter := range outputFilter {
-		for key, value := range inputFilter {
+		for key, value := range testFilter {
 			if *filter.Name == key && *filter.Values[0] == value {
 				foundMap[key] = true
+			}
+		}
+	}
+
+	for k, v := range foundMap {
+		if !v {
+			t.Fatalf("Fail: should have found value for key: %s", k)
+		}
+	}
+}
+
+func TestStepSourceAmiInfo_BuildFilter_ListValue(t *testing.T) {
+	filter_key := "name-no-space-between-comma"
+	filter_value := "foo1-1,foo1-2,foo1-3"
+	filter_key2 := "name-space-between-comma"
+	filter_value2 := "foo2-1, foo2-2, foo2-3"
+	filter_key3 := "name-embedded-comma-and-leading-space"
+	filter_value3 := "\"foo3-1, with comma\",foo3-2 without comma, \" foo3-3\""
+
+	inputFilter := map[string]string{
+		filter_key:  filter_value,
+		filter_key2: filter_value2,
+		filter_key3: filter_value3,
+	}
+
+	outputFilter := buildEc2Filters(inputFilter)
+
+	testFilter := map[string][]string{
+		filter_key:  {"foo1", "foo1-2", "foo1-3"},
+		filter_key2: {"foo2-1", "foo2-2", "foo2-3"},
+		filter_key3: {"foo3-1, with comma", "foo3-2 without comma", " foo3-3"},
+	}
+
+	// deconstruct filter back into things we can test
+	foundMap := map[string]bool{filter_key: false, filter_key2: false}
+	for _, filter := range outputFilter {
+		for key, value := range testFilter {
+			if *filter.Name == key {
+				for idx, filter_value := range value {
+					if *filter.Values[idx] == filter_value {
+						foundMap[key] = true
+					} else {
+						foundMap[key] = false
+					}
+				}
 			}
 		}
 	}
