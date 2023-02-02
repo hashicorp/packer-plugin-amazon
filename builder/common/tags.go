2da69a2f5a3994d26d4dec9ad9dfad2f7fc42283
Glyphack <sh.hooshyari@gmail.com>
2023-02-03 00:01:39 +0100

    iam: attach tags to instance role and profile
    RunTags value is used to set tags for resources created by packer except
    for Iam role and profile. This PR adds the tags when creating these
    resources.
    Resolves #322

--- builder/common/tags.go
+++ builder/common/tags.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go/service/iam"
 	"github.com/hashicorp/packer-plugin-sdk/multistep"
 	packersdk "github.com/hashicorp/packer-plugin-sdk/packer"
 	"github.com/hashicorp/packer-plugin-sdk/packerbuilderdata"
@@ -41,6 +42,28 @@ func (t TagMap) EC2Tags(ictx interpolate.Context, region string, state multistep
 		})
 	}
 	return ec2Tags, nil
+}
+
+func (t TagMap) IamTags(ictx interpolate.Context, region string, state multistep.StateBag) ([]*iam.Tag, error) {
+	var iamTags []*iam.Tag
+	generatedData := packerbuilderdata.GeneratedData{State: state}
+	ictx.Data = extractBuildInfo(region, state, &generatedData)
+
+	for key, value := range t {
+		interpolatedKey, err := interpolate.Render(key, &ictx)
+		if err != nil {
+			return nil, fmt.Errorf("Error processing tag: %s:%s - %s", key, value, err)
+		}
+		interpolatedValue, err := interpolate.Render(value, &ictx)
+		if err != nil {
+			return nil, fmt.Errorf("Error processing tag: %s:%s - %s", key, value, err)
+		}
+		iamTags = append(iamTags, &iam.Tag{
+			Key:   aws.String(interpolatedKey),
+			Value: aws.String(interpolatedValue),
+		})
+	}
+	return iamTags, nil
 }
 
 func (t EC2Tags) TagSpecifications(resourceType ...string) []*ec2.TagSpecification {
