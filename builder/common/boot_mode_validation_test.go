4aeb33d870e9d80cacc6e6f6c8dd79c5960dce18
Lucas Bajolet <lucas.bajolet@hashicorp.com>
2023-03-20 16:43:14 -0400

    common: add support for uefi-preferred boot mode
    In addition to legacy-bios and uefi, AWS supports also a third option
    for boot modes: uefi-preferred.
    
    This was not an accepted value before, and was requested by users, so we
    add this value to the list of accepted values.

---
+++ builder/common/boot_mode_validation_test.go
@@ -0,0 +1,44 @@
+package common
+
+import "testing"
+
+func TestIsValidBuildMode(t *testing.T) {
+	tests := []struct {
+		name        string
+		value       string
+		expectError bool
+	}{
+		{
+			"Valid value legacy-bios",
+			"legacy-bios",
+			false,
+		},
+		{
+			"Valid value uefi",
+			"uefi",
+			false,
+		},
+		{
+			"Valid value uefi-preferred",
+			"uefi-preferred",
+			false,
+		},
+		{
+			"Invalid value uefipreferred",
+			"uefipreferred",
+			true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := IsValidBootMode(tt.value)
+			if (err != nil) != tt.expectError {
+				t.Errorf("error mismatch, expected %t, got %t", tt.expectError, err != nil)
+				if err != nil {
+					t.Logf("got error: %s", err)
+				}
+			}
+		})
+	}
+}
